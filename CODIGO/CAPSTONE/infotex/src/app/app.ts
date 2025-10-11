a355789d7f662ebbf9f804c2c5fa5908c1ec11e0
Rena-Puente <re.varela@duocuc.cl>
2025-10-11 18:05:46 -0300

    icon

--- CODIGO/CAPSTONE/infotex/src/app/app.ts
+++ CODIGO/CAPSTONE/infotex/src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Infotex');
