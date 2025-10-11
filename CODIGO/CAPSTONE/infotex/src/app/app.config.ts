789d0ce9fe4d875e5c516dd034b0193c57ced674
Rena-Puente <re.varela@duocuc.cl>
2025-10-11 05:57:36 -0300

    debuf

--- CODIGO/CAPSTONE/infotex/src/app/app.config.ts
+++ CODIGO/CAPSTONE/infotex/src/app/app.config.ts
@@ -4,6 +4,7 @@ import {
   provideZonelessChangeDetection,
   isDevMode,
 } from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
