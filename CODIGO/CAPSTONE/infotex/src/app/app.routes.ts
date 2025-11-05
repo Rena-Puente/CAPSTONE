35aa89e825c8a2027a081f1afe290563e1608e42
Rena-Puente <127886423+Rena-Puente@users.noreply.github.com>
2025-11-05 20:07:34 -0300

    feat: add company onboarding page

--- CODIGO/CAPSTONE/infotex/src/app/app.routes.ts
+++ CODIGO/CAPSTONE/infotex/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Welcome } from './pages/welcome/welcome';
 import { Profile } from './pages/profile/profile';
+import { CompanyCreate } from './pages/company/company-create';
 import { authGuard } from './guards/auth.guard';
 import { guestGuard } from './guards/guest.guard';
 import { GithubCallback } from './pages/auth/github-callback/github-callback';
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'home', component: Home, canActivate: [authGuard] },
   { path: 'about', component: About, canActivate: [authGuard] },
   { path: 'profile', component: Profile, canActivate: [authGuard] },
+  { path: 'companies/create', component: CompanyCreate, canActivate: [authGuard] },
   { path: 'welcome', component: Welcome, canActivate: [guestGuard] },
   { path: 'auth/github/callback', component: GithubCallback },
   {
