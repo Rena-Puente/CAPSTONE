a6d127e4d7974b5c4d1f37f794169e15d1002a37
Rena-Puente <re.varela@duocuc.cl>
2025-11-20 09:44:21 -0300

    tabs

--- CODIGO/CAPSTONE/infotex-mobile/src/app/tabs/tabs.page.ts
+++ CODIGO/CAPSTONE/infotex-mobile/src/app/tabs/tabs.page.ts
@@ -1,5 +1,12 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
 
@@ -8,7 +15,7 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   standalone: true,
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
