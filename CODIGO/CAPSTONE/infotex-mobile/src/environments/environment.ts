23856e9576d90ec46b8b75ff11de8fb650199b53
Rena-Puente <127886423+Rena-Puente@users.noreply.github.com>
2025-11-19 23:15:45 -0300

    Configure API base URLs for authentication

--- CODIGO/CAPSTONE/infotex-mobile/src/environments/environment.ts
+++ CODIGO/CAPSTONE/infotex-mobile/src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://infotex.cl.ngrok.pizza'
 };
+
+export const API_AUTH_BASE = `${environment.apiUrl}/api/auth`;
 
 /*
  * For easier debugging in development mode, you can import the following file
