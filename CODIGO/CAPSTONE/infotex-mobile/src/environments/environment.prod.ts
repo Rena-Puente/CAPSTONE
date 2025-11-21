f205f36f4e694a70b373af1a615e68352b904750
Rena-Puente <re.varela@duocuc.cl>
2025-11-21 13:58:31 -0300

    ambiente

--- CODIGO/CAPSTONE/infotex-mobile/src/environments/environment.prod.ts
+++ CODIGO/CAPSTONE/infotex-mobile/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://infotex.cl.ngrok.pizza',
 };
 
-export const API_BASE = `${environment.apiUrl}/api`;
+export const API_BASE = environment.apiUrl;
 export const API_AUTH_BASE = `${API_BASE}/auth`;
--- CODIGO/CAPSTONE/infotex-mobile/src/environments/environment.ts
+++ CODIGO/CAPSTONE/infotex-mobile/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiUrl: 'https://infotex.cl.ngrok.pizza',
 };
 
-export const API_BASE = `${environment.apiUrl}/api`;
+export const API_BASE = environment.apiUrl;
 export const API_AUTH_BASE = `${API_BASE}/auth`;
 
 /*
